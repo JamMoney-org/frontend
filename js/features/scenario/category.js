0ff1149f468fbb12cbfb364a1cda0a1117cc9e98
boboyoii <139124147+boboyoii@users.noreply.github.com>
2025-05-24 14:50:48 +0900

    chore: 상황 리스트.html 파일명 변경 반영

--- js/features/scenario/category.js
+++ js/features/scenario/category.js
@@ -9,7 +9,7 @@ function getCategoryImagePath(name) {
 
 // 클릭 시 카테고리별 시나리오 리스트 페이지로 이동
 function navigateToScenarioList(category) {
-  location.href = `/pages/scenario_select.html?category=${encodeURIComponent(
+  location.href = `/pages/scenario_list.html?category=${encodeURIComponent(
     category
   )}`;
 }
