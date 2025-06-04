c1991ff0b59dd3bb70bfb211ce8fec690bc7e9c2
boboyoii <139124147+boboyoii@users.noreply.github.com>
2025-06-04 19:11:16 +0900

    fix: 업종분류 dto 변경

--- js/features/mock_invest/mock-info.js
+++ js/features/mock_invest/mock-info.js
@@ -78,7 +78,7 @@ function renderCompanyInfo() {
       : ''
   );
   setValue('시장정보구분(투자주의)', '해당없음');
-  setValue('업종분류', companyData.industry);
+  setValue('업종분류', companyData.bstp_kor_isnm);
   setValue('관리종목', 'N');
   setValue('거래정지', 'N');
   setValue(
