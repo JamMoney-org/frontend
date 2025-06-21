3b51722321f6175b304f01e7051897dccbb97cd9
Sohee Lee <essen030216@naver.com>
2025-06-21 15:23:35 +0900

    refactor:: login.html → index.html 이름 변경

--- js/features/vocabulary/vocabulary-quiz-result.js
+++ js/features/vocabulary/vocabulary-quiz-result.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // 데이터 유효성 검사
     if (!result || typeof result.correctCount !== "number" || typeof result.totalQuestions !== "number") {
         alert("결과 데이터를 불러오는 데 실패했습니다.");
-        window.location.href = "index.html";  // 오류 시 메인으로 이동
+        window.location.href = "mainpage.html";  // 오류 시 메인으로 이동
         return;
     }
 
