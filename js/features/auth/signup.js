785a4ad3611ea8e79349b2af35112067cf4742cb
seeun <rlatpdms0513@naver.com>
2025-06-22 16:13:39 +0900

    fix: json 수정

--- js/features/auth/signup.js
+++ js/features/auth/signup.js
@@ -33,7 +33,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-
                     email,
                     nickname,
                     password,
